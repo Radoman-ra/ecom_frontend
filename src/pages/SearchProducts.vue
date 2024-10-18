ad2eb13bc40ef863a25e4eb43317a57452194639
Radoman-ra <rmaksim886@gmail.com>
2024-10-18 20:19:09 +0200

    Refactor image loading logic in SearchProducts.vue

--- src/pages/SearchProducts.vue
+++ src/pages/SearchProducts.vue
@@ -74,7 +74,7 @@
                 <img
                   :src="loading ? product.lowQltyImgUrl : product.imageUrl"
                   alt="Product Image"
-                  @load="product.loading = false"
+                  @load="loading = false"
                   class="product-image"
                   :class="{ blurred: loading }"
                 />
@@ -154,7 +154,7 @@
               :src="loading ? selectedProduct.lowQltyImgUrl : selectedProduct.imageUrl"
               alt="Product Image"
               @load="loading = false"
-              class="product.loading = false"
+              class="modal-product-image"
               :class="{ blurred: loading }"
             />
             <div class="modal-info">
@@ -222,7 +222,6 @@ export default defineComponent({
         availableQuantity: number
         lowQltyImgUrl: string
         imageUrl: string
-        loading: boolean
         description: string
       }>,
       currentPage: 1,
@@ -238,7 +237,6 @@ export default defineComponent({
         description: string
         imageUrl: string
         lowQltyImgUrl: string
-        loading: true
       },
       cart: reactive(JSON.parse(localStorage.getItem('cart') || '[]'))
     }
