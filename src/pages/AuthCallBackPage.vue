093f523d1577751226aaaa0a4aca3757c4a2785a
Radoman-ra <rmaksim886@gmail.com>
2024-10-22 20:05:01 +0200

    Add AuthCallBackPage component and update router configuration

---
+++ src/pages/AuthCallBackPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Redirecting...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const urlParams = new URLSearchParams(window.location.search)
+    const accessToken = urlParams.get('access_token')
+    const refreshToken = urlParams.get('refresh_token')
+
+    if (accessToken && refreshToken) {
+      localStorage.setItem('access_token', accessToken)
+      localStorage.setItem('refresh_token', refreshToken)
+
+      this.$router.push('/')
+    } else {
+      console.error('Tokens are missing from URL')
+    }
+  }
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,15 @@ import Login from '../pages/LoginPage.vue'
 import Register from '../pages/RegisterPage.vue'
 import Cart from '../pages/CartPage.vue'
 import Profile from '../pages/ProfilePage.vue'
+import AuthCallback from '../pages/AuthCallBackPage.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/cart', component: Cart },
-  { path: '/profile', component: Profile }
+  { path: '/profile', component: Profile },
+  { path: '/auth/callback', component: AuthCallback }
 ]
 
 const router = createRouter({
