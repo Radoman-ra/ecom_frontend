fe3e3741e340c66fce9412ea89461684f3cb5d90
Radoman-ra <rmaksim886@gmail.com>
2024-10-22 20:13:03 +0200

    Refactor AuthCallBackPage to use Vue 3 composition API and router
    This commit refactors the AuthCallBackPage component to use the Vue 3 composition API and the vue-router package. It replaces the previous options API with the defineComponent function and the setup function. The code also updates the logic for saving access and refresh tokens to local storage and redirecting the user to the home page. This change improves code readability and maintainability.

--- src/pages/AuthCallBackPage.vue
+++ src/pages/AuthCallBackPage.vue
@@ -4,21 +4,36 @@
   </div>
 </template>
 
-<script>
-export default {
-  mounted() {
-    const urlParams = new URLSearchParams(window.location.search)
-    const accessToken = urlParams.get('access_token')
-    const refreshToken = urlParams.get('refresh_token')
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
 
-    if (accessToken && refreshToken) {
-      localStorage.setItem('access_token', accessToken)
-      localStorage.setItem('refresh_token', refreshToken)
+export default defineComponent({
+  setup() {
+    const router = useRouter()
 
-      this.$router.push('/')
-    } else {
-      console.error('Tokens are missing from URL')
+    const saveTokensAndRedirect = () => {
+      const urlParams = new URLSearchParams(window.location.search)
+      const accessToken = urlParams.get('access_token')
+      const refreshToken = urlParams.get('refresh_token')
+
+      if (accessToken && refreshToken) {
+        localStorage.setItem('access_token', accessToken)
+        localStorage.setItem('refresh_token', refreshToken)
+
+        router.push('/')
+      } else {
+        console.error('Tokens are missing from URL')
+      }
     }
+
+    return {
+      saveTokensAndRedirect
+    }
+  },
+
+  mounted() {
+    this.saveTokensAndRedirect()
   }
-}
+})
 </script>
