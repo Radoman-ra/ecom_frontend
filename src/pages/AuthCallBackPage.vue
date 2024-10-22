20dd97e277b914211f9678b895e3f34fe539282e
Radoman-ra <rmaksim886@gmail.com>
2024-10-22 20:21:00 +0200

    Refactor AuthCallBackPage to use Vue 3 composition API and router

--- src/pages/AuthCallBackPage.vue
+++ src/pages/AuthCallBackPage.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
+import Cookies from 'js-cookie'
 
 export default defineComponent({
   setup() {
@@ -16,10 +17,21 @@ export default defineComponent({
       const urlParams = new URLSearchParams(window.location.search)
       const accessToken = urlParams.get('access_token')
       const refreshToken = urlParams.get('refresh_token')
+      const ACCESS_TOKEN_EXPIRE_MINUTES = 15
+      const REFRESH_TOKEN_EXPIRE_DAYS = 7
+      console.log(accessToken)
+      console.log(refreshToken)
 
       if (accessToken && refreshToken) {
-        localStorage.setItem('access_token', accessToken)
-        localStorage.setItem('refresh_token', refreshToken)
+        Cookies.set('access_token', accessToken, {
+          expires: ACCESS_TOKEN_EXPIRE_MINUTES / (24 * 60),
+          secure: true
+        })
+
+        Cookies.set('refresh_token', refreshToken, {
+          expires: REFRESH_TOKEN_EXPIRE_DAYS,
+          secure: true
+        })
 
         router.push('/')
       } else {
