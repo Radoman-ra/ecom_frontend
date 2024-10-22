fb8a86c989a99d6ac55abce17ac2b77ffb740585
Radoman-ra <rmaksim886@gmail.com>
2024-10-22 21:05:19 +0200

    Refactor avatar upload endpoint URL in ProfilePage.vue

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -159,7 +159,7 @@ export default defineComponent({
         }
 
         const response = await axios.post(
-          `${import.meta.env.VITE_BACKEND_URL}/profile/avatar`,
+          `${import.meta.env.VITE_BACKEND_URL}/profile/avatar/upload`,
           formData,
           {
             headers: {
